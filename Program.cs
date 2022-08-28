aadb6782c869ccff9c8b8af0588a7a45fd6971bd
Anastasiia Tribunskaia <nasta-kuz91@mail.ru>
2022-08-28 14:12:19 +0300

    говно

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Hello,  ");
+Console.WriteLine(username);
+Console.Write("иди на хер ");
+Console.WriteLine(username);
